f588da0a94146074b5edc99296bb21505bb8db10
Marcelo <Marcelo96mh@hotmail.com>
2020-12-14 02:00:30 -0500

    project init structure
    add file: config.rs, main.rs, models.rs

---
+++ todomvcBackendRust/src/main.rs
@@ -0,0 +1,30 @@
+mod config;
+mod models;
+
+use crate::models::Status;
+use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
+use std::io;
+
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok().json(Status {
+        status: "up".to_string(),
+    })
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    println!(
+        "Starting server port http://{}:{}",
+        config.server.host, config.server.port
+    );
+
+    HttpServer::new(|| App::new().route("/", web::get().to(status)))
+        .bind(format!("{}:{}", config.server.host, config.server.port))?
+        .run()
+        .await
+}
